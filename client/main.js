dc0f42f50930d2a1f121714033bd7f84b497961c
Jake <adam.falck@gmail.com>
2019-07-21 12:48:23 -0500

    June 21st, 2019 added portfolio link to navbar

--- client/main.js
+++ client/main.js
@@ -24,29 +24,55 @@ $(document).ready(function(){
   })
 })
 
+// removed contact form js logic 7/20/2019
 
-Template.main.events({
+//Template.main.events({
 
 
 // jquery contact form find data itmems
 
-    'submit #contactForm': function(event){
-    event.preventDefault();
-    var element = $(event.target);
-    var name = element.find("#name").val();
-    var email = element.find("#email").val();
-    var message = element.find("#message").val();
+  //   'submit #contactForm': function(event){
+  //   event.preventDefault();
+  //   var element = $(event.target);
+  //   var name = element.find("#name").val();
+  //   var email = element.find("#email").val();
+  //   var message = element.find("#message").val();
+  //
+  //   //create object to pass to serve methods
+  //   var subForm = {
+  //     name: name,
+  //     email: email,
+  //     message: message,
+  //   };
+  //
+  //   // call meteor to send email from input
+  //   Meteor.call('sendEmail', subForm);
+  //   Router.go("/contact");
+  // },
+//});   // end of remove 7/20/2019
 
-    //create object to pass to serve methods
-    var subForm = {
-      name: name,
-      email: email,
-      message: message,
-    };
+//// Facebook API call (added 7/20/2019 AF, JS)
 
-    // call meteor to send email from input
-    Meteor.call('sendEmail', subForm);
-    Router.go("/contact");
+Template.footer.onCreated(function () {
+  this.feed = new ReactiveVar();
+
+  var link = "https://graph.facebook.com/v2.11/isightrpv?fields=posts.limit(3)"
+  var arguments = {
+    headers: { "User-Agent": "Meteor/1.1" },
+    params: {
+      "access_token": "956271767862418|2XmW6Fr58RZKChHtB8m20AhOSJ8",
+    }
+  };
+  Meteor.http.call('GET', link, arguments, function (error, response) {
+    this.feed.set(response.data);
+  }.bind(this));
+});
+
+Template.footer.helpers({
+  feed1: function () {
+    return Template.instance().feed.get().posts.data[0].message;
   },
-
+  feed2: function () {
+    return Template.instance().feed.get().posts.data[1].message;
+  }
 });
