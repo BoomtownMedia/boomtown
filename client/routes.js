03706b1ff2259b408544c93eeaf48b5ec9ec8493
Adam Falck <adam.falck@gmail.com>
2019-09-18 12:06:42 -0500

    privacy policy

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,11 @@
-Router.route('/', function() {
-  this.render('main');
+Router.route("/", function() {
+  this.render("main");
 });
 
-Router.route('/mission', function(){
-  this.render('mission');
+Router.route("/mission", function() {
+  this.render("mission");
+});
+
+Router.route("/privacy_policy", function() {
+  this.render("privacy_policy");
 });
